bd16c5739213cf7484a01a033ed39bf2e99c2d53
gita <gita@date4dos.co.il>
2022-12-03 22:28:38 +0200

    added dbounce

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,3 +1,21 @@
+const dbounce = (calllback, time) => {
+  let x;
+  return (...arg) => {
+    clearTimeout(x);
+    x = setTimeout(() => {
+      calllback(...arg);
+    }, time);
+  };
+};
+
 export default () => {
-  return <div>Home</div>;
+  const f = (num) => {
+    setTimeout(() => {
+      console.log("end server call" + num);
+    }, 1000);
+  };
+
+  const myD = dbounce(f, 500);
+
+  return <div onClick={()=>myD(7897987)}>Home</div>;
 };
